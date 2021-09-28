2b1d0c51dc6875d539373f846de55ed09b58174a
Christofer <christoferguima@gmail.comnam>
2021-09-27 23:37:38 -0300

    adding table and calculate function

---
+++ script.js
@@ -0,0 +1,61 @@
+const form = document.querySelector("form");
+const product = document.querySelector("#product");
+const amount = document.querySelector("#amount");
+const price = document.querySelector("#price");
+const add = document.querySelector("#add");
+const calc = document.querySelector("#calc");
+const cart = document.querySelector("#cart");
+const tbody = document.querySelector("tbody");
+const balance = document.querySelector('#total');
+const allItens = [];
+let id = 0;
+
+const addProduct = () => {
+  const item = {};
+
+  id++;
+  item.id = String(id);
+  item.name = product.value;
+  item.price = price.value;
+  item.amount = amount.value;
+
+  return item;
+};
+
+const addProductTable = () => {
+  tbody.innerText = "";
+
+  for (let i = 0; i < allItens.length; i++) {
+    let tr = tbody.insertRow();
+
+    let td_id = tr.insertCell();
+    let td_product = tr.insertCell();
+    let td_price = tr.insertCell();
+    let td_amount = tr.insertCell();
+
+    td_id.innerText = allItens[i].id;
+    td_product.innerText = allItens[i].name;
+    td_price.innerText = allItens[i].price;
+    td_amount.innerText = allItens[i].amount;
+
+    td_id.classList.add("center");
+    td_amount.classList.add("center");
+  }
+};
+
+//add product on click
+add.addEventListener("click", () => {
+  allItens.push(addProduct());
+
+  addProductTable();
+  console.log(allItens);
+});
+
+calc.addEventListener("click", () => {
+
+  const map = allItens.map((item) => item.amount * item.price); // multiplicar amount pelo preço
+
+  const reduce = map.reduce((accumulator, item) => accumulator + item); // somar todos os preços
+
+  balance.innerHTML = `Total: R$ <strong>${reduce.toFixed(2)}</strong>`
+});
